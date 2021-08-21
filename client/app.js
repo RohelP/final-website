0fdd1e5e2e0f210212a80fddf4b32bb2d4d08822
Rohel <pereirarohel21@gmail.com>
2021-08-21 05:38:27 -0400

    update

--- client/app.js
+++ client/app.js
@@ -1,1 +1,12 @@
-const menu = documents.querySelector('')
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const navLogo = document.querySelector('#navbar_logo')
+const body = document.querySelector('body')
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.toggle('active')
+    body.classList.toggle('active')
+}
+
+menu.addEventListener('click', mobileMenu)
